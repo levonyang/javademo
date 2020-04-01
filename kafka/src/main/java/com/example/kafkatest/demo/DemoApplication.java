ac3d934f2f78395aa0bb01ab9065519df23295a8
zhangxuepei <zhangxuepei@hikvision.com.cn>
2020-04-01 20:03:08 +0800

    demo 更新

--- kafka/src/main/java/com/example/kafkatest/demo/DemoApplication.java
+++ kafka/src/main/java/com/example/kafkatest/demo/DemoApplication.java
@@ -1,7 +1,9 @@
 package com.example.kafkatest.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaAdmin;
 
 @SpringBootApplication
 public class DemoApplication {
