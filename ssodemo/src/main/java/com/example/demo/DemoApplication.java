a164f92f3576832499ef685d2841e4254ca2b60c
zhangxuepei <zhangxuepei@hikvision.com.cn>
2020-04-11 14:58:28 +0800

    demo更新

--- ssodemo/src/main/java/com/example/demo/DemoApplication.java
+++ ssodemo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
